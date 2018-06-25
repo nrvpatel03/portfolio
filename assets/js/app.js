960afcef4b728778e7a8993a6e3fb374eb45f5bd
Nirav Patel <nrvpatel03@gmail.com>
2018-06-25 09:38:15 -0700

    scrollNav working

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,9 +12,34 @@ function openNav() {
 function closeNav() {
     $("#mySidenav").css("width", "0");
 }
-
+// function fadeAnimationIn (posWin,eleScroll,id){
+//     if(posWin === eleScroll){
+//         $(id).show();
+//     }
+// }
+// function fadeAnimationOut (posWin, eleScroll, id){
+//     if(posWin > eleScroll+10){
+//         $(id).removeClass("animated fadeOut");
+//     }
+// }
+function navScroll(idNav,idScrollto){
+    $(idNav).click(function(){
+        $('html, body').animate({
+            scrollTop: ($(idScrollto).offset().top - ($(".navbar").height() + 15))
+        },500);
+    })
+}
 $(document).ready(function(){
     //get pos of all divs/elements
+    // const aboutme = $("#aboutDiv").offset().top-$("#aboutDiv").height();
+    // const skills = $("#skillsDiv").offset().top;
+    // const projHeadT = $("#portfolio-headText").offset().top;
+    // const testProj = $("#testProj").offset().top;
+    // $("#aboutDiv").hide();
+    navScroll("#aboutNav","#aboutDiv");
+    navScroll("#skillsNav","#skillsDiv");
+    navScroll("#projectNav","#portfolio-headText");
+    navScroll("#contactNav","#contact-headText");
     
     $(window).scroll(function(){
         const pos = $(window).scrollTop();
@@ -23,6 +48,9 @@ $(document).ready(function(){
         }else{
         $(".navbar").css("background-color","rgb(162, 57, 202, 1)");
         }
+        // console.log(aboutme);
+        // console.log(pos);
+        // fadeAnimationIn(pos,aboutme,"#aboutDiv");
     });
     
 });
